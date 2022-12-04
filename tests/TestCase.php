946f2b47bb1dc524295f11140a593257d8ec0f65
z.mtrn <zmatrianpour@gmail.com>
2022-12-04 18:25:35 +0330

    test init

---
+++ tests/TestCase.php
@@ -0,0 +1,27 @@
+<?php 
+namespace Mtrn\ApiService\Tests;
+
+use Mtrn\ApiService\ApiServiceServiceProvider;
+use Orchestra\Testbench\TestCase as TestbenchTestCase;
+
+
+class TestCase extends TestbenchTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        // additional setup
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            ApiServiceServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        // perform environment setup
+    }
+}
